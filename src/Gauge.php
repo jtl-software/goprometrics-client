331189389d432578631e0951182f1586b7dc4756
avermeulen <andre.vermeulen@jtl-software.com>
2020-07-02 10:04:50 +0200

    Add set method to gauge

--- src/Gauge.php
+++ src/Gauge.php
@@ -97,19 +97,39 @@ class Gauge
      * @param string $help
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function sendRequest(
+    public function set(
         string $namespace,
         string $name,
         float $value,
         LabelList $tagList = null,
         string $help = ''
     ): void {
-        $tagStr = (string) $tagList;
+        $this->sendRequest($namespace, $name, $value, $tagList, $help, true);
+    }
+
+    /**
+     * @param string $namespace
+     * @param string $name
+     * @param float $value
+     * @param LabelList|null $tagList
+     * @param string $help
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function sendRequest(
+        string $namespace,
+        string $name,
+        float $value,
+        LabelList $tagList = null,
+        string $help = '',
+        bool $useSet = false
+    ): void {
+        $tagStr = (string)$tagList;
+        $useSetStr = $useSet ? '1' : 0;
         $this->client->request(
             'PUT',
             "{$this->baseUrl}/gauge/{$namespace}/{$name}/{$value}",
             [
-                'body' => "labels={$tagStr}&help={$help}",
+                'body' => "labels={$tagStr}&help={$help}&useSet={$useSetStr}",
                 'headers' => ['Content-Type' => "application/x-www-form-urlencoded"]
             ]
         );
