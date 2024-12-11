974bbccd063e07c10867ac73148df849c8c66d61
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2024-12-11 14:47:20 +0100

    EA-6816: add add-Parameter to increase counter by custom value (for php8.1 support)

--- src/Counter.php
+++ src/Counter.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace JTL\GoPrometrics\Client;
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 
 /**
  * This File is part of JTL-Software
@@ -24,18 +26,24 @@ class Counter
     /**
      * @param string $namespace
      * @param string $name
-     * @param LabelList $tagList
+     * @param LabelList|null $tagList
      * @param string $help
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @param float $add
+     * @throws GuzzleException
      */
-    public function count(string $namespace, string $name, LabelList $tagList = null, string $help = ''): void
-    {
-        $tagStr = (string) $tagList;
+    public function count(
+        string $namespace,
+        string $name,
+        LabelList $tagList = null,
+        string $help = '',
+        float $add = 1.0
+    ): void {
+        $tagStr = (string)$tagList;
         $this->client->request(
             'PUT',
             "{$this->baseUrl}/count/{$namespace}/{$name}",
             [
-                'body' => "labels={$tagStr}&help={$help}",
+                'body' => "labels={$tagStr}&help={$help}&add={$add}",
                 'headers' => ['Content-Type' => "application/x-www-form-urlencoded"]
             ]
         );
